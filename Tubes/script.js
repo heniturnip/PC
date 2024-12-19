6e7f2042e6843d5bafc672f821457ced239c9883
heniturnip <heni.121140080@student.itera.ac.id>
2024-12-19 15:34:23 +0700

    update

---
+++ Tubes/script.js
@@ -0,0 +1,5 @@
+document.getElementById('kontak-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Pesan Anda telah terkirim!');
+    // Anda bisa menambahkan kode untuk mengirim pesan ke server di sini
+});
